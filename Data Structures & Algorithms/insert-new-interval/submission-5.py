5a099b2f4ab1a8ca8ff6531dd00a3dbee4ae1193
Johan Jomy <67508310+JohanJomy@users.noreply.github.com>
2026-06-30 00:30:53 +0530

    Add: insert-new-interval - submission-5

---
+++ Data Structures & Algorithms/insert-new-interval/submission-5.py
@@ -0,0 +1,31 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        s = newInterval[0]
+        l, r = 0, len(intervals) - 1
+
+        while l <= r:
+            mid = (l+r)//2
+            if intervals[mid][0] < s:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        i = l
+
+        if i > 0 and intervals[i-1][1] >= newInterval[0]:
+            newInterval = [intervals[i-1][0], max(intervals[i-1][1], newInterval[1])]
+            i -= 1
+            intervals.pop(i)
+        # print(intervals, newInterval, i, i < len(intervals)-1)
+
+        while i <= len(intervals)-1 and newInterval[1] >= intervals[i][0]:
+            # print(True)
+            newInterval = [newInterval[0], max(intervals[i][1], newInterval[1])]
+            intervals.pop(i)
+        
+        intervals.insert(i, newInterval)
+
+        return intervals
+
+
+            
