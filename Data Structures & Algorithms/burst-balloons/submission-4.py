01e6b0d5c196fff490ac6125e55c2fc2718fd745
Johan Jomy <67508310+JohanJomy@users.noreply.github.com>
2026-07-07 23:04:57 +0530

    Add: burst-balloons - submission-4

---
+++ Data Structures & Algorithms/burst-balloons/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums = [1] + nums + [1]
+
+        # nums.insert(n, 1)
+        # nums.insert(0, 1)
+
+        dp = [[-1] * n for _ in range(n)]
+        
+        def dfs(l, r):
+            # consider poping the ith elemnet last then make to subarrays and find result
+            if l > r:
+                return 0
+
+            if dp[l-2][r-2] != -1:
+                return dp[l-2][r-2]
+            
+            dp[l-2][r-2] = 0
+            for i in range(l, r+1):
+                # pop the ith element last
+                dp[l-2][r-2] = max(dp[l-2][r-2], 
+                    nums[l-1] * nums[i] * nums[r+1] +
+                    dfs(i+1, r) +
+                    dfs(l, i-1)
+                )
+            
+
+
+            return dp[l-2][r-2]
+        
+        return dfs(1, n)
+        
