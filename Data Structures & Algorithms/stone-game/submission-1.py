dd0f8c342251c7ab97aa7d76d7ad9b2b515d632f
Johan Jomy <67508310+JohanJomy@users.noreply.github.com>
2026-08-16 11:51:03 +0530

    Add: stone-game - submission-1

---
+++ Data Structures & Algorithms/stone-game/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        dp = {} # (l, r) = max alice total
+
+        def dfs(l, r):
+            if l == r:
+                # last game i.e bobs turn (only one choice)
+                return 0
+            
+            if (l, r) in dp:
+                return dp[(l, r)]
+            
+            turn = (l-r) % 2
+
+            if turn:
+                # alice
+                left = piles[l] + dfs(l+1, r)
+                right = piles[r] + dfs(l, r-1)
+
+                dp[(l, r)] = max(left, right)
+            else:
+                dp[(l, r)] = max(dfs(l+1, r), dfs(l, r-1))
+
+            return dp[(l, r)]
+
+
+        total = sum(piles)
+        #bob = total-alice
+
+        alice =  dfs(0, len(piles)-1)
+
+        return alice > (total-alice)
+
